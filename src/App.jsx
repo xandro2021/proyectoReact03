701dc954adc799ccd573293582fde4bf4e061f8b
xandro2021 <xandrocastro2021@gmail.com>
2023-10-06 16:39:41 -0600

    save the user inputs in a useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styled from '@emotion/styled';
 import Formulario from './components/Formulario';
 import ImagenCripto from './img/imagen-criptos.png';
@@ -43,6 +43,15 @@ const Heading = styled.h1`
 
 function App() {
 
+  const [monedas, setMonedas] = useState({});
+
+  useEffect(() => {
+    if (Object.keys(monedas).length > 0) {
+      alert(monedas.moneda);
+    }
+  }, [monedas]);
+
+
   return (
     <Contenedor>
 
@@ -54,7 +63,9 @@ function App() {
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
 
-        <Formulario />
+        <Formulario
+          setMonedas={setMonedas}
+        />
       </div>
 
     </Contenedor>
